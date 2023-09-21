3d35e9630d8b8806d8df78f6c06f29627dc41145
VictorIdowu <idsonvictor@gmail.com>
2023-09-21 08:25:32 +0100

    Changed login background image and fixed popUp for mobile

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -53,7 +53,7 @@ const LoginForm = (props) => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8 md:gap-12 items-center backdrop-blur-2xl backdrop-hue-rotate-90 backdrop-saturate-200 z-40 px-5 py-8 w-64 sm:w-1/2 lg:w-5/12 rounded-xl"
+      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8 md:gap-12 items-center backdrop-blur-2xl backdrop-hue-rotate-180 backdrop-saturate-200 z-40 px-5 py-8 w-64 sm:w-1/2 lg:w-5/12 rounded-xl"
     >
       <h3 className="flex items-center justify-between text-secondary-100 font-semibold text-sm sm:text-lg md:text-xl w-full">
         Welcome Back
@@ -81,14 +81,14 @@ const LoginForm = (props) => {
           placeholder="•••••••••"
         />
       </label>
-      <aside className="w-full">
+      <aside className="w-full transition-all duration-300">
         {!buttonLoader && (
-          <button className="bg-secondary-100 text-center text-primary-300 w-full rounded-md py-2 sm:py-3 md:py-6 text-sm sm:text-lg md:text-xl">
+          <button className="bg-secondary-100 hover:bg-secondary-200 text-center text-primary-300 w-full rounded-md py-2 sm:py-3 md:py-6 text-sm sm:text-lg md:text-xl transition-colors duration-500">
             Log In
           </button>
         )}
         {buttonLoader && (
-          <button className="bg-secondary-100 text-center text-primary-300 w-full rounded-md py-2 sm:py-3 md:py-6 text-lg sm:text-xl md:text-2xl ">
+          <button className="bg-secondary-200 text-center text-primary-300 w-full rounded-md py-2 sm:py-3 md:py-6 text-lg sm:text-xl md:text-2xl ">
             <Icon
               icon="mingcute:loading-line"
               className="w-full animate-spin"
--- src/components/Login/PopUp.jsx
+++ src/components/Login/PopUp.jsx
@@ -1,7 +1,7 @@
 const PopUp = (props) => {
   return (
     <aside
-      className={`fixed gap-2 sm:gap-5 w-fit -top-10 -right-20 bg-secondary-100 shadow-[rgba(32,30,30,0.75)] shadow-2xl  z-50  flex flex-col justify-between rounded-lg overflow-hidden animate-bounce transition-opacity`}
+      className={`fixed gap-2 sm:gap-5 w-fit -top-10 md:-right-20 bg-secondary-100 shadow-[rgba(32,30,30,0.75)] shadow-2xl  z-50  flex flex-col justify-between rounded-lg overflow-hidden animate-bounce transition-opacity`}
     >
       <h2 className="text-base sm:text-lg md:text-2xl text-primary-300 px-3 sm:px-10 py-4">
         {props.msg}
