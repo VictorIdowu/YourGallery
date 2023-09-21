bfad5da4d38b84157715bef6d59e7927990d326f
VictorIdowu <idsonvictor@gmail.com>
2023-09-21 11:19:32 +0100

    some

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="flex justify-between py-3 text-secondary-200 items-start px-3 sm:px-6 md:px-14 relative">
+    <header className="flex justify-between py-3 mt-3 text-secondary-200 items-start px-3 sm:px-6 md:px-14 relative">
       <h2 className="text-px sm:text-lg md:text-xl lg:text-2xl font-bold">
         iDsOn GalLErY
       </h2>
